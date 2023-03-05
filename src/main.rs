fec838b75fa2afd8f521558c2d0f995ce564e376
Shubham <shivam828787@gmail.com>
2023-03-06 02:31:19 +0530

    some refctoring

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,84 @@
 use tokio::net::UdpSocket;
-use std::net::{Ipv4Addr, SocketAddr, IpAddr};
+use std::{net::{Ipv4Addr, SocketAddr, IpAddr}, u16};
 use log;
 mod cli;
 use env_logger;
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
-use tokio_tun::TunBuilder;
+use tokio_tun::{TunBuilder, Tun};
+use rand::Rng;
 
-async fn conn (remote_addr: String, port: u16) {
-    log::info!("Starting as client");
-    let tun = TunBuilder::new()
-    .name("")
-    .address(Ipv4Addr::new(10,0,0,5))
-    .netmask(Ipv4Addr::new(255,255,255,0))         // if name is empty, then it is set by kernel.
-    .tap(false)   // false (default): TUN, true: TAP.
-    .packet_info(false)  // false: IFF_NO_PI, default is true.
-    .up()                // or set it up manually using `sudo ip link set <tun-name> up`.
-    .try_build();
+struct Node {
+    id: String,
+    tun: Option<Tun>
+}
 
-    let tun = match tun {
-        Ok(tun) => tun,
-        Err(_err) => { panic!("yo") }
-    };
-    let (mut reader, mut writer) = tokio::io::split(tun);
+impl Node {
+    fn new(id: &str, ip: Ipv4Addr) -> Node {
+        let tun = TunBuilder::new()
+        .name("")
+        .address(ip)
+        .netmask(Ipv4Addr::new(255,255,255,0))         // if name is empty, then it is set by kernel.
+        .tap(false)   // false (default): TUN, true: TAP.
+        .packet_info(false)  // false: IFF_NO_PI, default is true.
+        .up()                // or set it up manually using `sudo ip link set <tun-name> up`.
+        .try_build();
+
+        let tun = match tun {
+            Ok(tun) => {
+                log::info!("initialized Node with Tun {:?}", ip);
+                tun
+            },
+            Err(_err) => { panic!("failed to intialized tun with error {:?}", _err) }
+        };
+        
+        Node {id: id.to_string(), tun: Some(tun)}
+    }
+}
+struct PeerNode {
+    me: Node,
+    turn: (Ipv4Addr, u16),
+}
+
+impl PeerNode {
+    fn new(id: &str, turn_ip: Ipv4Addr, turn_port: u16) -> Self {
+        let mut rng = rand::thread_rng();
+        let ip = Ipv4Addr::new(10,0,0,rng.gen_range(10..254));
+        let node = Node::new(id, ip);
+        PeerNode {me: node, turn: (turn_ip, turn_port)}
+    }
+
+    fn broadcast() {
+        unimplemented!()
+    }
+}
+
+struct Turn {
+    me: Node,
+    nodes: Vec<Node>
+}
+
+impl Turn {
+    fn new(id: &str) -> Self {
+        let ip = Ipv4Addr::new(10,0,0,5);
+        let node = Node::new(id, ip);
+        Turn {me: node, nodes: Vec::new()}
+    }
+    fn add_node( &mut self, node: Node) {
+        self.nodes.push(node);
+    }
+}
+
+
+async fn conn (turn_addr: String, port: u16) {
+    log::info!("Starting as peer node");
+    let [a,b,c,d] : [u8; 4] = turn_addr.split('.')
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<u8>>()
+        .try_into()
+        .unwrap();
+    let node = PeerNode::new("hostname", Ipv4Addr::new(a,b,c,d), port);
+
+    let (mut reader, mut writer) = tokio::io::split(node.me.tun.unwrap());
     let socket = UdpSocket::bind("0.0.0.0:8080").await;
     let socket = match socket {
         Ok(socket) => socket,
@@ -29,7 +86,7 @@ async fn conn (remote_addr: String, port: u16) {
     };
     let mut buf1 = [0u8; 1500];
     let mut buf2 = [0u8; 1500];
-    let raddr = remote_addr + ":" + &port.to_string();
+    let raddr = _addr + ":" + &port.to_string();
     let mut sock_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),8080);
     let mut len = 0;
     let mut len1 = 0;
@@ -58,7 +115,7 @@ async fn conn (remote_addr: String, port: u16) {
 }
 
 async fn serv (port: &u16)  {
-    log::info!("Starting as server");
+    log::info!("Starting as server node");
     let tun = TunBuilder::new()
     .name("")
     .address(Ipv4Addr::new(10,0,0,6))
@@ -81,21 +138,22 @@ async fn serv (port: &u16)  {
         Err(_err) => { panic!("yo") }
     };
     let mut buf2 = [0u8; 1500];
-    let mut buf1 = [0u8; 1500];
+  //  let mut buf1 = [0u8; 1500];
     let mut caddr: String = "null".to_string();
-    let mut len = 0;
     let mut len1 = 0;
     loop {
         tokio::select! {
-            _ = async {
+            (buf, len) = async {
+                let mut buf = [0u8; 1500];
+                let mut len = 0;
                 log::debug!("reading from interface");
-                len = reader.read(&mut buf1).await.expect("error while reading from interface");
-                log::debug!("interface - {:?}", &buf1[0..len]);
-            } => {
+                len = reader.read(&mut buf).await.expect("error while reading from interface");
+                log::debug!("interface - {:?}", &buf[0..len]);
+                (buf[0..len].to_vec(), len) } => {
                     log::debug!("sending to client without addr");
                     if caddr != String::from("null") {
                         log::debug!("sending to client {}", &caddr);
-                        socket.send_to(&buf1[0..len], &caddr).await.expect("error in writting to socket"); 
+                        socket.send_to(&buf[0..len], &caddr).await.expect("error in writting to socket"); 
                     }        
                 },
             _ = async {
@@ -126,26 +184,4 @@ async fn main() {
         cli::Args::Client(client) => conn(client.remote_addr, client.port).await,
         cli::Args::Server(server) => serv(&server.port).await,
     }
-    // let mut dev = create_tun("10.0.0.2".to_string());
-    // let mut dev1 = create_tun("10.1.0.2".to_string());
-	// let mut buf = [0; 1024];
-
-    // let socket = UdpSocket::bind("localhost:8080").expect("couldn't bind to address");
-
-    // let socket1 = UdpSocket::bind("127.0.0.1:8081").expect("couldn't bind to address");
-
-	// loop {
-	// 	let amount = dev.read(&mut buf).unwrap();
-	// 	println!("received from tap is - {:?}", &buf[0 .. amount]);
-    //     socket.send_to(&buf, "127.0.0.1:8081");
-
-    //     socket1.recv_from(&mut buf).unwrap();
-    //     print!("at server {:?}", &buf);
-    //     dev1.write(&mut buf).unwrap();
-    //     let amoun = dev1.read(&mut buf ).unwrap();
-    //     socket1.send_to(&buf, "127.0.0.1:8080");
-    //     socket.recv_from(&mut buf).unwrap();
-    //     dev.write(&mut buf).unwrap();
-
-	// }
 }
