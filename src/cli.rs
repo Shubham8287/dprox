b59a27fabf3ea4ec4fe10455f6b9aa48d9cb4e69
Shubham <shivam828787@gmail.com>
2023-03-21 16:43:15 +0530

    removed comments from cli

--- src/cli.rs
+++ src/cli.rs
@@ -3,18 +3,13 @@ use clap::{App, Arg, SubCommand};
 
 #[derive(Debug, Clone)]
 pub struct Server {
-    // pub bind_addr: String,
     pub port: u16,
-    // pub key: String,
-    // pub dns: IpAddr,
 }
 
 #[derive(Debug, Clone)]
 pub struct Client {
     pub remote_addr: String,
     pub port: u16,
-    // pub key: String,
-    // pub default_route: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -44,22 +39,7 @@ pub fn get_args() -> Result<Args, String> {
                         .default_value("8080")
                         .help("set the listen port")
                         .takes_value(true),
-                ),
-                // .arg(
-                //     Arg::with_name("key")
-                //         .short("k")
-                //         .long("key")
-                //         .help("set the key for encryption communication")
-                //         .takes_value(true),
-                // )
-                // .arg(
-                //     Arg::with_name("dns")
-                //         .short("d")
-                //         .long("dns")
-                //         .default_value("8.8.8.8")
-                //         .help("set dns for client, default 8.8.8.8")
-                //         .takes_value(true),
-                // ),
+                )
         )
         .subcommand(
             SubCommand::with_name("client")
@@ -77,20 +57,7 @@ pub fn get_args() -> Result<Args, String> {
                         .long("port")
                         .help("set the remote port")
                         .takes_value(true),
-                ),
-                // .arg(
-                //     Arg::with_name("key")
-                //         .short("k")
-                //         .long("key")
-                //         .help("set the key for encryption communication")
-                //         .takes_value(true),
-                // )
-                // .arg(
-                //     Arg::with_name("no-default-route")
-                //         .short("n")
-                //         .long("no-default-route")
-                //         .help("do not set default route"),
-                // ),
+                )
         )
         .get_matches();
     if let Some(matches) = matches.subcommand_matches("client") {
@@ -102,46 +69,19 @@ pub fn get_args() -> Result<Args, String> {
             .value_of("port")
             .ok_or_else(|| "can not find client port value")
             .unwrap();
-        // let key_str = matches
-        //     .value_of("key")
-        //     .ok_or_else(|| "can not find client key value")
-        //     .unwrap();
-        // let remote_addr = IpAddr::V4(Ipv4Addr::from_str(ip_str).map_err(|e| e.to_string())?);
         let port = port_str.parse::<u16>().map_err(|e| e.to_string())?;
-        // let default_route = match matches.is_present("no-default-route") {
-        //     false => true,
-        //     true => false,
-        // };
         Ok(Args::Client(Client {
             remote_addr: ip_str.to_string(),
             port: port,
-            // key: key_str.to_string(),
-            // default_route: default_route,
         }))
     } else if let Some(matches) = matches.subcommand_matches("server") {
-        // let ip_str = matches
-        //     .value_of("bind")
-        //     .ok_or_else(|| "can not find server host value")
-        //     .unwrap();
         let port_str = matches
             .value_of("port")
             .ok_or_else(|| "can not find server port value")
             .unwrap();
-        // let key_str = matches
-        //     .value_of("key")
-        //     .ok_or_else(|| "can not find server key value")
-        //     .unwrap();
-        // let dns = matches
-        //     .value_of("dns")
-        //     .ok_or_else(|| "can not find dns value")?;
-        // let bind_addr = IpAddr::V4(Ipv4Addr::from_str(ip_str).map_err(|e| e.to_string())?);
-        // let dns = IpAddr::V4(Ipv4Addr::from_str(dns).map_err(|e| e.to_string())?);
         let port = port_str.parse::<u16>().map_err(|e| e.to_string())?;
         Ok(Args::Server(Server {
-            // bind_addr: ip_str.to_string(),
             port: port,
-            // key: key_str.to_string(),
-            // dns: dns,
         }))
     } else {
         unimplemented!()
