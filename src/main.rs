50ca161ac44257f510d5ae9cdefe1bf48ab001af
Shubham <shivam828787@gmail.com>
2023-03-31 09:45:50 +0530

    restructured bit

--- src/main.rs
+++ src/main.rs
@@ -2,115 +2,11 @@ use log;
 use std::{net::Ipv4Addr, str::from_utf8, u16};
 use tokio::net::UdpSocket;
 mod cli;
+mod model;
 use env_logger;
 use rand::Rng;
-use serde::ser::{Serialize, SerializeStruct, Serializer};
 use std::collections::HashMap;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::time::{self, Duration};
-use tokio_tun::{Tun, TunBuilder};
-
-struct Node {
-    id: u8,
-    tun: Option<Tun>,
-}
-
-impl Node {
-    fn new(id: u8, ip: Ipv4Addr) -> Node {
-        let tun = TunBuilder::new()
-            .name("")
-            .address(ip)
-            .netmask(Ipv4Addr::new(255, 255, 255, 0)) // if name is empty, then it is set by kernel.
-            .tap(false) // false (default): TUN, true: TAP.
-            .packet_info(false) // false: IFF_NO_PI, default is true.
-            .up() // or set it up manually using `sudo ip link set <tun-name> up`.
-            .try_build();
-
-        let tun = match tun {
-            Ok(tun) => {
-                log::info!("initialized Node with Tun {:?}", ip);
-                tun
-            }
-            Err(_err) => {
-                panic!("failed to intialized tun with error {:?}", _err)
-            }
-        };
-
-        Node {
-            id: id,
-            tun: Some(tun),
-        }
-    }
-}
-struct PeerNode {
-    me: Node,
-    turn: (Ipv4Addr, u16),
-}
-
-impl PeerNode {
-    fn new(turn_ip: Ipv4Addr, turn_port: u16) -> Self {
-        let mut rng = rand::thread_rng();
-        let last_oct: u8 = rng.gen_range(10..254);
-        let ip = Ipv4Addr::new(10, 0, 0, last_oct.clone());
-        let node = Node::new(last_oct, ip);
-        PeerNode {
-            me: node,
-            turn: (turn_ip, turn_port),
-        }
-    }
-
-    async fn heartbeat(turn: &(Ipv4Addr, u16), id: u8, localport :u16) {
-        let socket = UdpSocket::bind(format!("0.0.0.0:{}", localport)).await.expect("unbale to create socket");
-        let mut interval = time::interval(Duration::from_secs(3));
-        let (ip, port) = turn;
-        socket
-            .connect(format!("{}:{}", ip.to_string(), port))
-            .await
-            .expect("failed to connect to server");
-        let mut buf = [0u8; 120];
-        buf[0] = 2;
-        buf[1] = id;
-        loop {
-            socket.send(&buf).await.expect("unable to send heartbeat");
-            interval.tick().await;
-        }
-    }
-}
-
-struct Turn {
-    me: Node,
-    nodes: HashMap<u8, String>,
-}
-
-impl Turn {
-    fn new() -> Self {
-        let ip = Ipv4Addr::new(10, 0, 0, 5);
-        let node = Node::new(5, ip);
-        let mut node_map: HashMap<u8, String> = HashMap::new();
-        node_map.insert(5, ip.to_string());
-        Turn {
-            me: node,
-            nodes: node_map,
-        }
-    }
-
-    fn add_node(&mut self, id: u8, node_addr: &String) {
-        self.nodes.insert(id, node_addr.clone());
-    }
-}
-
-impl Serialize for Turn {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("Turn", 2)?;
-        state.serialize_field("me", &self.me.id)?;
-        state.serialize_field("nodes", &self.nodes)?;
-
-        state.end()
-    }
-}
 
 async fn conn(turn_addr: String, port: u16) {
     log::info!("Starting as peer node");
@@ -122,13 +18,13 @@ async fn conn(turn_addr: String, port: u16) {
         .try_into()
         .unwrap();
 
-    let mut node = PeerNode::new(Ipv4Addr::new(a, b, c, d), port);
+    let mut node = model::PeerNode::new(Ipv4Addr::new(a, b, c, d), port);
     let turn = node.turn;
     let id = node.me.id;
     let mut rng = rand::thread_rng();
     let localport = rng.gen_range(9000..9100);
     tokio::task::spawn(async move {
-        PeerNode::heartbeat(&turn, id, localport).await;
+        model::PeerNode::heartbeat(&turn, id, localport).await;
         });
     let tun = node.me.tun.as_mut();
     let (mut reader, mut writer) = tokio::io::split(tun.unwrap());
@@ -163,7 +59,7 @@ async fn conn(turn_addr: String, port: u16) {
 
 async fn serv(port: &u16) {
     log::info!("Starting as server node");
-    let mut node = Turn::new();
+    let mut node = model::Turn::new();
     let map_ref = &mut (node.nodes);
     let tun = (node.me.tun).as_mut().unwrap();
     let (mut reader, mut writer) = tokio::io::split(tun);
---
+++ src/model.rs
@@ -0,0 +1,111 @@
+
+use log;
+use std::{net::Ipv4Addr, u16};
+use tokio::net::UdpSocket;
+use rand::Rng;
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+use std::collections::HashMap;
+use tokio::time::{self, Duration};
+use tokio_tun::{Tun, TunBuilder};
+
+pub struct Node {
+    pub id: u8,
+    pub tun: Option<Tun>,
+}
+
+impl Node {
+    fn new(id: u8, ip: Ipv4Addr) -> Node {
+        let tun = TunBuilder::new()
+            .name("")
+            .address(ip)
+            .netmask(Ipv4Addr::new(255, 255, 255, 0)) // if name is empty, then it is set by kernel.
+            .tap(false) // false (default): TUN, true: TAP.
+            .packet_info(false) // false: IFF_NO_PI, default is true.
+            .up() // or set it up manually using `sudo ip link set <tun-name> up`.
+            .try_build();
+
+        let tun = match tun {
+            Ok(tun) => {
+                log::info!("initialized Node with Tun {:?}", ip);
+                tun
+            }
+            Err(_err) => {
+                panic!("failed to intialized tun with error {:?}", _err)
+            }
+        };
+
+        Node {
+            id: id,
+            tun: Some(tun),
+        }
+    }
+}
+pub struct PeerNode {
+    pub me: Node,
+    pub turn: (Ipv4Addr, u16),
+}
+
+impl PeerNode {
+    pub fn new(turn_ip: Ipv4Addr, turn_port: u16) -> Self {
+        let mut rng = rand::thread_rng();
+        let last_oct: u8 = rng.gen_range(10..254);
+        let ip = Ipv4Addr::new(10, 0, 0, last_oct.clone());
+        let node = Node::new(last_oct, ip);
+        PeerNode {
+            me: node,
+            turn: (turn_ip, turn_port),
+        }
+    }
+
+    pub async fn heartbeat(turn: &(Ipv4Addr, u16), id: u8, localport :u16) {
+        let socket = UdpSocket::bind(format!("0.0.0.0:{}", localport)).await.expect("unbale to create socket");
+        let mut interval = time::interval(Duration::from_secs(3));
+        let (ip, port) = turn;
+        socket
+            .connect(format!("{}:{}", ip.to_string(), port))
+            .await
+            .expect("failed to connect to server");
+        let mut buf = [0u8; 120];
+        buf[0] = 2;
+        buf[1] = id;
+        loop {
+            socket.send(&buf).await.expect("unable to send heartbeat");
+            interval.tick().await;
+        }
+    }
+}
+
+pub struct Turn {
+    pub me: Node,
+    pub nodes: HashMap<u8, String>,
+}
+
+impl Turn {
+    pub fn new() -> Self {
+        let ip = Ipv4Addr::new(10, 0, 0, 5);
+        let node = Node::new(5, ip);
+        let mut node_map: HashMap<u8, String> = HashMap::new();
+        node_map.insert(5, ip.to_string());
+        Turn {
+            me: node,
+            nodes: node_map,
+        }
+    }
+
+    fn add_node(&mut self, id: u8, node_addr: &String) {
+        self.nodes.insert(id, node_addr.clone());
+    }
+}
+
+impl Serialize for Turn {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Turn", 2)?;
+        state.serialize_field("me", &self.me.id)?;
+        state.serialize_field("nodes", &self.nodes)?;
+
+        state.end()
+    }
+}
